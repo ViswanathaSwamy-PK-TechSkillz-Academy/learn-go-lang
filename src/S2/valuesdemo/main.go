bd06084f0365ab2c434cf6dca3aa93d63b0dc602
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-24 14:17:00 +0530

    Update main.go

--- src/S2/valuesdemo/main.go
+++ src/S2/valuesdemo/main.go
@@ -9,4 +9,17 @@ import (
 func main() {
 	fmt.Println("Showing the Values Demo")
 
+	fmt.Println("Hello World")
+	fmt.Println(`Hello World`)
+
+	fmt.Println("This is first line. \nThis is second line.")
+	fmt.Println(`This is first line. \nThis is second line.`)
+
+	/*
+		.\main.go:18:36: newline in string || .\main.go:18:36: syntax error: unexpected newline in argument list; possibly missing comma or )
+		.\main.go:19:25: newline in string
+	*/
+	fmt.Println("This is first line. This is second line.")
+	fmt.Println(`This is first line. 
+	This is second line.`)
 }
