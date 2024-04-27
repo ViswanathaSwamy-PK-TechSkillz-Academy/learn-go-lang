249dedf7f0b9b5f63c41c8694cb37a309d3e46ed
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-27 20:30:54 +0530

    Update transfer.go

--- src/MP1/Transfers/transfer.go
+++ src/MP1/Transfers/transfer.go
@@ -6,23 +6,18 @@ import "time"
 type Transfer struct {
 	id        string
 	date      time.Time
-	amount    float64
+	amount    *Amount
 	accountId int64
 }
 
 // NewTransfer is the only way to create a transfer entity
-func NewTransfer(id string, date time.Time, amount float64, accountId int64) *Transfer {
+func NewTransfer(id string, date time.Time, amount float64, currency string, accountId int64) *Transfer {
 	return &Transfer{
 		id:        id,
 		date:      date,
-		amount:    amount,
+		amount:    NewAmount(amount, currency),
 		accountId: accountId,
 	}
-}
-
-// IsAmountLimitAllowed check whether amount to transfer does not got beyond the bank limits
-func (t *Transfer) IsAmountLimitAllowed() bool {
-	return t.amount < 10000
 }
 
 // IsDateAllowed checks whether transfer is executed in the current day
@@ -30,4 +25,9 @@ func (t *Transfer) IsDateAllowed() bool {
 	now := time.Now()
 
 	return !t.date.After(now)
+}
+
+// IsAmountLimitAllowed check whether amount to transfer does not got beyond the bank limits
+func (t *Transfer) IsAmountLimitAllowed() bool {
+	return t.amount.IsAmountLimitAllowed()
 }
