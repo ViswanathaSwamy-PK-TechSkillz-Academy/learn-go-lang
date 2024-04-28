af5fd512f284ebd6c9027df033c4ebb45d06f062
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-28 10:32:33 +0530

    Swamy/27apr work (#24)
    * Create amount.go
    
    * Update amount.go
    
    * Update transfer.go
    
    * Update transfer.go
    
    * Update main.go

--- src/MP1/Transfers/transfer.go
+++ src/MP1/Transfers/transfer.go
@@ -6,33 +6,28 @@ import "time"
 type Transfer struct {
 	id        string
 	date      time.Time
-	amount    float64
+	amount    *Amount
 	accountId int64
 }
 
 // NewTransfer is the only way to create a transfer entity
-func NewTransfer(id string, date time.Time, amount float64, accountId int64) *Transfer {
+func NewTransfer(id string, date time.Time, amount float64, currency string, accountId int64) *Transfer {
 	return &Transfer{
 		id:        id,
 		date:      date,
-		amount:    amount,
+		amount:    NewAmount(amount, currency),
 		accountId: accountId,
 	}
-}
-
-// IsAmountLimitAllowed check whether amount to transfer does not got beyond the bank limits
-func (t *Transfer) IsAmountLimitAllowed() bool {
-	if t.amount >= 10000 {
-		return false
-	}
-	return true
 }
 
 // IsDateAllowed checks whether transfer is executed in the current day
 func (t *Transfer) IsDateAllowed() bool {
 	now := time.Now()
-	if t.date.After(now) {
-		return false
-	}
-	return true
+
+	return !t.date.After(now)
+}
+
+// IsAmountLimitAllowed check whether amount to transfer does not got beyond the bank limits
+func (t *Transfer) IsAmountLimitAllowed() bool {
+	return t.amount.IsAmountLimitAllowed()
 }
--- src/MP1/main.go
+++ src/MP1/main.go
@@ -10,22 +10,22 @@ import (
 func main() {
 
 	time := time.Now()
-	transfer := transfers.NewTransfer("1234", time, 12000.7, 44354363)
+	transfer := transfers.NewTransfer("1234", time, 12000.7, "USD", 44354363)
 
-	fmt.Printf("*** Amount limit validation *** \n")
-	// Validate amount
-	if transfer.IsAmountLimitAllowed() {
-		fmt.Println("Amount is allowed")
-	} else {
-		fmt.Println("Amount is not allowed")
-	}
-
-	fmt.Printf("\n\n *** Date validation: *** \n")
+	fmt.Printf("\n*** Date validation: ***\n")
 	// Validate date
 	if transfer.IsDateAllowed() {
 		fmt.Println("Date is allowed")
 	} else {
 		fmt.Println("Date is not allowed")
+	}
+
+	fmt.Printf("\n\n*** Amount limit validation *** \n")
+	// Validate amount
+	if transfer.IsAmountLimitAllowed() {
+		fmt.Println("Amount is allowed")
+	} else {
+		fmt.Println("Amount is not allowed")
 	}
 
 }
